344b8bcfef9a64438ac909126357074a8fc39bdd
Tanzeel12345 <113383596+Tanzeel12345@users.noreply.github.com>
2024-08-27 22:37:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,9 @@ if ingredients_List:
 
         
     #st.write(ingredients_string)
-    
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+
+       my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """"')"""
 
 
